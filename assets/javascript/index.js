11e9df43cf72613935abd89abd785a81b1a1d568
Cezar Carvalhaes <cezarcarvalhaes@gmail.com>
2018-06-08 13:08:46 -0400

    fixed switched links

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -190,8 +190,8 @@ $(document).ready(function () {
         });
         $("#info-header").text("Netscrape")
         $("#info-text").text("A news site developed with Mongoose and Cheerio that scrapes articles from Allsides.com. Users can save articles and post comments.")
-        $("#weblink").attr("href", "https://cezarcarvalhaes.github.io/netscrape")
-        $("#gitlink").attr("href", "https://netscrape-navigator.herokuapp.com/")
+        $("#weblink").attr("href", "https://netscrape-navigator.herokuapp.com/")
+        $("#gitlink").attr("href", "https://cezarcarvalhaes.github.io/netscrape")
     }
     
 
